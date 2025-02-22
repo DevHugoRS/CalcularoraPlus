5b58470a7aba286434ee8f7519b18d3e4dca1bde
DevHugoRS <eng.hugo.rs@gmail.com>
2025-02-22 10:51:08 +0100

    Inicio

---
+++ CalculadoraPlus/CalculadoraPlus/CalculadoraPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraPlusApp.swift
+//  CalculadoraPlus
+//
+//  Created by Hugo Rodrigues on 22/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
